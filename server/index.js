1311372f213e4cc904cca3eade5151e129fe820e
trivien11vn <triviencoderr@gmail.com>
2024-06-14 18:21:36 +0700

    save connection

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
-
+require('dotenv').config();
+const cors = require('cors');
+const dbconnect = require('./config/dbconn');
 const app = express()
+//middleware
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}))
+app.use(express.json());
 
-app.use('/', (req, res)=>{res.send('SERVER ON')})
+app.use(express.urlencoded({
+    extended: true
+}))
 
-app.listen(5000, ()=>{console.log('Server on port 5000')})
+dbconnect()
+
+const PORT = process.env.PORT || 7777
+app.listen(PORT, ()=>{console.log('Server on port ' + PORT)})
