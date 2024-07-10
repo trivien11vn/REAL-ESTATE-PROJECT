0975460243d391ef98e76a442227410b1acf3220
trivien11vn <triviencoderr@gmail.com>
2024-07-10 09:31:27 +0700

    fix: update center in MapContainer

--- client/vite-project/src/components/Map/Map.jsx
+++ client/vite-project/src/components/Map/Map.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { apiGetLongitudeAndLatitudeFromAddress } from 'src/apis/beyond'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 const attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 const url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -12,12 +12,12 @@ const Map = ({address='', zoom=12}) => {
     const fetchCenter = async () => {
       const response = await apiGetLongitudeAndLatitudeFromAddress(address)
       if (response.status === 200 && response?.data?.features?.length > 0) {
-        console.log('222')
         setCenter([
           response?.data?.features[0]?.geometry?.coordinates[1],
           response?.data?.features[0]?.geometry?.coordinates[0]
         ])
-      } else {
+      } 
+      else {
         window.navigator.geolocation.getCurrentPosition((position) => { 
           setCenter([position?.coords?.latitude, position?.coords?.longitude])
         })
@@ -25,10 +25,10 @@ const Map = ({address='', zoom=12}) => {
     }
 
     if (address) {
-      console.log('111')
       fetchCenter()
     } else {
       window.navigator.geolocation.getCurrentPosition((position) => { 
+        console.log(position)
         setCenter([position?.coords?.latitude, position?.coords?.longitude])
       })
     }
@@ -38,22 +38,35 @@ const Map = ({address='', zoom=12}) => {
     console.log('Center updated:', center)
   }, [center])
 
+const FlyMapTo = () => {
+
+  const map = useMap()
+
+  useEffect(() => {
+      map.setView(center)
+  }, [center])
+
+  return null
+}
+
   return (
     <div className='w-full h-[300px]'>
-      {center &&  
+      {center &&
       <MapContainer
-        center={center}
-        zoom={zoom}
-        className='w-full h-full'
-        scrollWheelZoom={false}
+      center={center}
+      zoom={zoom}
+      className='w-full h-full'
+      scrollWheelZoom={false}
       >
         <TileLayer url={url} attribution={attribution} />
         <Marker position={center}>
           <Popup>
-            {address}
+            A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
-      </MapContainer>}
+        <FlyMapTo />
+      </MapContainer>
+      }
     </div>
   )
 }
